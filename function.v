d78edce7d7d40cce2bd7534445c48eb6f5c6a77f
riyan-eng <fbriyant11@gmail.com>
2023-12-30 22:57:29 +0700

    feat(function)

--- function.v
+++ function.v
@@ -1,13 +1,13 @@
+fn add(x int, y int)int{
+	return x+y
+}
+
 fn main(){
 	println('text')
 	println("penjumlahan")
 	println(add(2,3))
 	println("pengurangan")
 	println(sub(2,3))
-}
-
-fn add(x int, y int)int{
-	return x+y
 }
 
 fn sub(x int,y int) int{
